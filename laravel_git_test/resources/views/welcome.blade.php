af47611a87a6d87e24490c7c987ab40c8b7fe6fd
nnirmall <vhandarinirmal@gmail.com>
2020-09-10 11:41:47 +0545

    changed welcome blade

--- laravel_git_test/resources/views/welcome.blade.php
+++ laravel_git_test/resources/views/welcome.blade.php
@@ -71,6 +71,7 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">NewLOgin</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
